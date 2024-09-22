db1dae1fd8937f40a821a18fb2a604a4ee6f1ff3
alexander-brady <bradya@student.ethz.ch>
2024-09-22 03:06:42 -0400

    remove prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,7 @@
-import { PrismaClient } from '@prisma/client'
 import Interface from './interface';
 
-const prisma = new PrismaClient()
-
-async function getPatient() {
-  let patient = await prisma.patientData.findFirst({
-      include: {
-          appointments: true
-      }
-  });
-
-  // If no patient is found, create one
-  if (!patient) {
-      await prisma.patientData.create({
-          data: {
-              first_name: 'Alice',
-              last_name: 'Johnson',
-
-              dob: new Date('1990-01-01'),
-              gender: 'F',
-
-              email: 'alice124356@gmail.com',
-
-              comments: 'Family history of shingles',
-              appointments: {
-                  create: {
-                      date: new Date('2022-01-01'),
-                      symptoms: 'Mild fever'
-                  },
-              },
-          }
-      })
-      patient = await prisma.patientData.findFirst({
-          include: {
-              appointments: true
-          }
-      });
-  }
-  return patient;
-}
-
-export default async function Home() {
-  const patient = await getPatient();
+export default function Home() {
+  const patient = { first_name: 'Alice' };
   return (
     <section className="flex flex-col sm:gap-2 sm:pb-20 h-full flex-1 w-1/2">
         <Interface firstname={patient?.first_name || 'Guest' } />
