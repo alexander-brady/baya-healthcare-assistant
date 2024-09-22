fbaf636ecc16194fe4833a3262ddd175f5d0b120
alexander-brady <bradya@student.ethz.ch>
2024-09-22 10:49:04 -0400

    try path changing

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/:path*'
-            : '/flask/',
+            : '/',
       },
     ]
  },
