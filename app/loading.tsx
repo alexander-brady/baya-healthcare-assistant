7e8bbe4c2b5667ba6ca3a554ef646a5d62211767
alexander-brady <bradya@student.ethz.ch>
2024-09-22 00:38:03 -0400

    loading

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <>
+            <div className='sm:pt-44'></div>
+            <h1 className="text-3xl font-semibold">Loading...</h1>
+            <span className="text-xl sm:pb-8">Please wait a moment.</span>
+        </>
+    )
+  }
